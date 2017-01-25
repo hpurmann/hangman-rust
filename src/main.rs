0d71cdb3485887d1bf34ac64f800d3687df6f42e
Hendrik Purmann <h.purmann@googlemail.com>
2017-01-25 23:21:38 +0100

    Store whether a char has been revealed
    We need to have always know whether a certain character has been reveiled.
    This solution is pretty pragmatic and not optimal.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,19 @@ use std::io;
 const DEBUG: bool = true;
 const MAXIMUM_TRIES: i8 = 7;
 
+struct Digit {
+    value: char,
+    revealed: bool,
+}
+
 fn main() {
     let solution: String = get_solution();
     debug(&solution);
 
-    let solution_vector = string_to_vector(&solution);
+    let solution_vector = init_digits(&solution);
+    for x in &solution_vector {
+        println!("Value: {}, Revealed: {}", x.value, x.revealed)
+    }
 
     for i in 0..MAXIMUM_TRIES {
         let mut guess = String::new();
@@ -31,11 +39,15 @@ fn debug(message: &String) {
     }
 }
 
-fn string_to_vector(string: &String) -> Vec<char> {
-    let mut vector: Vec<char> = Vec::new();
+fn init_digits(string: &String) -> Vec<Digit> {
+    let mut vector: Vec<Digit> = Vec::new();
     let str = string.as_str();
     for my_char in str.chars() {
-        vector.push(my_char);
+        let current_digit = Digit {
+            value: my_char,
+            revealed: false,
+        };
+        vector.push(current_digit);
     }
     return vector;
 }
