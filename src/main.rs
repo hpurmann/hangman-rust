f766c7072a802512259c1a9b1a94713247963b2a
Hendrik Purmann <h.purmann@googlemail.com>
2017-01-25 22:16:54 +0100

    Fix vectors and iterators

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,7 @@ fn main() {
     let solution: String = get_solution();
     debug(&solution);
 
-    let solution_vector = string_to_vector(solution);
-    //for x in &solution_vector {
-        //print!("{} ", x);
-    //}
+    let solution_vector = string_to_vector(&solution);
 
     for i in 0..MAXIMUM_TRIES {
         let mut guess = String::new();
@@ -34,12 +31,11 @@ fn debug(message: &String) {
     }
 }
 
-fn string_to_vector(string: String) -> Vec<char> {
-    let mut vector: Vec<char>;
-    let vector2: Vec<char> = vec!['a', 'o'];
-    //print!("{}", vector[0]);
-    for i in 0..string.len() {
-        print!("{}", &string[i..i+1]);
+fn string_to_vector(string: &String) -> Vec<char> {
+    let mut vector: Vec<char> = Vec::new();
+    let str = string.as_str();
+    for my_char in str.chars() {
+        vector.push(my_char);
     }
-    return vector2;
+    return vector;
 }
