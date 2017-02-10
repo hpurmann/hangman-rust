4fd15f06630178a23d2a0691ceafd5531f8e3b70
Hendrik Purmann <h.purmann@googlemail.com>
2017-02-10 08:59:56 +0100

    Handle error for no internet connection

--- src/words.rs
+++ src/words.rs
@@ -4,20 +4,21 @@ extern crate hyper;
 extern crate rand;
 extern crate spinner;
 
+use std::error::Error;
 use std::io::Read;
 use self::hyper::{Client};
 use self::rand::Rng;
 use self::spinner::{SpinnerBuilder};
 
 #[derive(Serialize, Deserialize)]
-struct Entry {
+struct Data {
     status: u32,
     total: u64,
-    results: Vec<Result>,
+    results: Vec<Entry>,
 }
 
 #[derive(Serialize, Deserialize)]
-struct Result {
+struct Entry {
     headword: String,
 }
 
@@ -29,33 +30,54 @@ fn get_content(url: &str) -> hyper::Result<String> {
     Ok(buf)
 }
 
-fn query(dict: &str, offset: u64, limit: i8) -> Entry {
-    let res = get_content(
+fn query(dict: &str, offset: u64, limit: i8) -> Result<Data, Box<Error>> {
+    let res = try!(get_content(
         format!(
             "http://api.pearson.com/v2/dictionaries/{}/entries?offset={}&limit={}",
             dict, offset, limit
             ).as_str()
-        );
-    return serde_json::from_str(res.unwrap().as_str()).unwrap();
+        ));
+    let data: Data = try!(serde_json::from_str(res.as_str()));
+    Ok(data)
+}
+
+fn get_static_answers() -> Vec<String> {
+    return vec![
+        "unimaginatively".into(),
+        "interestingly".into(),
+        "random".into(),
+        "unfair".into(),
+        "computer".into(),
+    ];
 }
 
 pub fn get_random() -> String {
     let dict = "lasde";
     let sp = SpinnerBuilder::new("Getting random english word from dictionary.".into()).start();
+    let static_answers = get_static_answers();
 
-    let entry1 = query(dict, 1, 1);
-    let size = entry1.total;
+    let query1 = query(dict, 1, 1);
+    let size = match query1 {
+        Ok(v) => v.total,
+        Err(_) => static_answers.len() as u64,
+    };
     let offset = rand::thread_rng().gen_range(0, size);
 
-    let entry2 = query(dict, offset, 1);
-    sp.message("Done!".into());
-    sp.close();
-
-    if entry2.status == 200 {
-        let solution: String = entry2.results.into_iter().nth(0).unwrap().headword;
-        return solution.to_uppercase();
-    }
-    println!("Request unsuccessful, returned code {}", entry2.status);
-    println!("Falling back to static string");
-    return "unimaginatively".to_string().to_uppercase();
+    let query2 = query(dict, offset, 1);
+    let solution: String = match query2 {
+        Ok(v) => {
+            sp.message("Done!".into());
+            sp.close();
+            let solution: String = v.results.into_iter().nth(0).unwrap().headword;
+            solution.to_uppercase()
+        },
+        Err(e) => {
+            println!("Error: {}", e);
+            sp.message("Request unsuccessful (no internet connection?)".into());
+            println!("Falling back to static string");
+            sp.close();
+            static_answers.get(offset as usize).unwrap().to_uppercase()
+        },
+    };
+    return solution;
 }
